0596d0f0b4d87fff85db817ed9bbdf423502e765
KrimiWajih <krimi.wajih@gmail.com>
2025-02-12 15:00:16 +0100

    Update config.js
    changed credentials

--- configuration/config.js
+++ configuration/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const config = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://krimiwajih:pAc9oszoTDZri84c@cluster0.jzz0p.mongodb.net/Movies_DB"
+      "mongodb+srv://name:password@cluster0.jzz0p.mongodb.net/Movies_DB"
     );
     console.log("Connected Successfully");
   } catch (error) {
